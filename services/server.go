25d3d8e3e32f2aa5ed33ee5e87fc574b8e9ae5d0
XiaoXin <xiaoxin@XiaoXindeMacBook-Pro.local>
2020-09-01 21:54:57 +0800

    update

--- services/server.go
+++ services/server.go
@@ -2,86 +2,56 @@ package services
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/libra412/auto_recharge/utils"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
-var api_url = "https://api.bdxxk.com/api/getApiOrder.json"
-var handle_url = "https://api.bdxxk.com/api/handleApiOrder.json"
+// "https://api.hbdm.com"如果无法访问请使用："https://api.btcgateway.pro"。
+var api_url = "https://api.hbdm.com"
+var api_url_bak = "https://api.btcgateway.pro"
 
-//
+// 获取行情
+func GetKLine(symbol, period string, params ...int) Response {
+	p := url.Values{}
+	p.Add("symbol", symbol)
+	p.Add("period", period)
+	if len(params) == 0 {
+		panic("参数不全")
+	}
+	if len(params) == 1 || len(params) == 3 {
+		p.Add("size", strconv.Itoa(params[0]))
+	}
+	if len(params) == 2 {
+		p.Add("from", strconv.Itoa(params[0]))
+		p.Add("to", strconv.Itoa(params[1]))
+	}
+	uri := `/market/history/kline`
+	resp, err := http.Get(api_url_bak + uri + "?" + p.Encode())
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	var response Response
+	json.Unmarshal(respBody, &response)
+	return response
+}
+
+// 返回内容
 type Response struct {
-	Issuccess   bool
-	Description string
-	Data        []TradeInfo
-}
-
-//
-type TradeInfo struct {
-	TradeId           int
-	ProductId         int
-	FacePrice         float32
-	SellPrice         float32
-	TargetAccount     string
-	TargetAccountType int
-	RechargeMode      int
-	RechargeModeName  string
-	BuyAmount         int
-	TotalSalePrice    float32
-	Area              string
-	Game              string
-	GameName          string
-	StockShopId       int
-	StockShopName     string
-	CustomerIp        string
-	CustomerRegion    string
-	DealDateTime      string
-}
-
-// 获取订单
-func GetData(merchantId, key string) []TradeInfo {
-	params := url.Values{}
-	params.Add("merchantId", merchantId)
-	params.Add("count", "100")
-	params.Add("sign", signMd5(merchantId+key))
-	resp, err := http.PostForm(api_url, params)
-	if err != nil {
-		fmt.Print(err)
-		return nil
+	Ch   string
+	Data []struct {
+		Id     int
+		Vol    float64
+		Count  float64
+		Open   float64
+		Close  float64
+		Low    float64
+		High   float64
+		Amount float64
 	}
-	defer resp.Body.Close()
-	responseBody, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(responseBody))
-	if responseBody != nil {
-		var v Response
-		json.Unmarshal(responseBody, &v)
-		return v.Data
-	}
-	return nil
-}
-
-//更新订单
-func UpdateData(merchantId, key, tradeId, state, stateInfo string) []byte {
-	params := url.Values{}
-	params.Add("merchantId", merchantId)
-	params.Add("tradeId", tradeId)
-	params.Add("status", state)
-	params.Add("description", stateInfo)
-	params.Add("sign", signMd5(merchantId+tradeId+state+key))
-	resp, err := http.PostForm(handle_url, params)
-	if err != nil {
-		fmt.Print(err)
-		return nil
-	}
-	defer resp.Body.Close()
-	responseBody, _ := ioutil.ReadAll(resp.Body)
-	return responseBody
-}
-
-//
-func signMd5(params string) string {
-	return utils.Md5([]byte(params))
+	Status string
+	Ts     string
 }
